fb65fd0708fbd41f319a78660e76a113f16cec13
Han Solo <han@ubuntu>
2013-02-04 21:54:30 -0500

    added stuff to text grader

--- crypt/proj/SubsitutionCypherSolver/src/cypherSolver/TextGrader.java
+++ crypt/proj/SubsitutionCypherSolver/src/cypherSolver/TextGrader.java
@@ -1,5 +1,46 @@
 package cypherSolver;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class TextGrader {
 
+	private static final String alphabet = "abcdefghijklmnopqrstuvwxyz";
+	private static final String lettersInOrderOfFrequency = "etaoinshrdlcumwfgypbvkjxqz";
+	private String text;
+	public TextGrader(String txt)
+	{
+		text =txt;
+	}
+	
+	public String getText()
+	{
+		return this.text;
+	}
+	
+	private HashMap<String,Integer> emptyLetterFrequencyTable()
+	{
+		HashMap<String,Integer> out = new HashMap<String,Integer>();
+		for(char letter:alphabet.toCharArray())
+		{
+			String temp ="";
+			temp += letter;
+			out.put(temp, 0);
+		}
+		return out;
+	}
+	
+	public HashMap<String,Integer> letterFrequency()
+	{
+		HashMap<String,Integer> out = emptyLetterFrequencyTable();
+		for(char letter:this.text.toCharArray())
+		{
+			String temp ="";
+			temp += letter;
+			out.put(temp,out.get(temp) + 1);
+		}
+		return out;
+		 
+	}
 }
