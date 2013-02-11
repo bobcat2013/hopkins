7b4e4e4109bdd18c4d2cbb6f0a8f008d8b60db6a
bobcat2013 <bobcat.yates@gmail.com>
2013-02-10 23:05:25 -0500

    adding the rest of the source

---
+++ crypt/proj/SubsitutionCypherSolver/src/cypherSolver/CypherSearcher.java
@@ -0,0 +1,26 @@
+package cypherSolver;
+
+import aStarSearch.AStarNode;
+import aStarSearch.AStarSearcher;
+import aStarSearch.ExploredNodeHolder;
+import aStarSearch.VistedNodeHolder;
+
+public class CypherSearcher {
+
+	public ExploredCypherNodeHolder ecnh;
+	public VistedNodeHolder vnh; 
+	public CypherSearcher(String inTxt)
+	{
+		ecnh = new ExploredCypherNodeHolder(inTxt);
+		vnh = new VistedNodeHolder();
+	}
+	public CypherNode step()
+	{
+		CypherNode best = this.ecnh.visitBestNode();
+		this.vnh.addNewNode(best);
+		return best;
+	}
+	
+
+	
+}
